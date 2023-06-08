32baaf7cc60dd6aa1ed7c5085fdeb0ae8a5725ce
SandunDinesh <135201246+SandunDinesh@users.noreply.github.com>
2023-06-08 10:56:55 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        InterfaceImplement obj=new InterfaceImplement();
+        obj.display();
+        obj.display1();
+    }
+}
---
+++ src/MyFirstInterface.java
@@ -0,0 +1,7 @@
+public interface MyFirstInterface {
+    public static final int x=0;  // In interface, there are no any difference between when we use public static final or not use it.Because in the interface, it already get variables as a public static final.
+    int y=10; // This Answer is also 0.
+    abstract void display();
+    public void display1(); // In the Interface, there are no any difference between when we use abstract keyword or not.Because in interface, it already get method as a abstract methods.
+
+}
