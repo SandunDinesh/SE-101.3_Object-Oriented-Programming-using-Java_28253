ec619bd69632946ad7af182d3c80c65e69b02edf
SandunDinesh <135201246+SandunDinesh@users.noreply.github.com>
2023-06-08 16:00:32 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        InterfaceImplement obj=new InterfaceImplement();
-        obj.display();
-        obj.display1();
-    }
-}
